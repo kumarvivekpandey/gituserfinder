676019a38dd08709367158ef173419c40a21d9f0
kumarvivekpandey <35356580+kumarvivekpandey@users.noreply.github.com>
2023-09-25 12:08:11 +0000

    final done
    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React, { useState, useEffect } from 'react';
-
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Chip from '@mui/material/Chip';
+import Alert from '@mui/material/Alert';
 const API_URL = "https://api.github.com/users/";
 
 const FetchUserData = () => {
   const [inputValue, setInputValue] = useState('');
   const [user, setUser] = useState('');
   const [result, setResult] = useState({});
-  
+
   useEffect(() => {
     if (user) {
-    
+
       fetch(API_URL + user)
         .then((res) => {
           if (!res.ok) {
@@ -20,7 +28,7 @@ const FetchUserData = () => {
         .then(data => setResult(data))
         .catch(error => console.error("Error fetching data:", error));
     }
-               
+
   }, [user]);
 
   const handleSubmit = (e) => {
@@ -29,8 +37,7 @@ const FetchUserData = () => {
     setUser(inputValue);
   };
 
-
-
+  
   return (
     <>
       <h1>Gituserfinder</h1>
@@ -40,22 +47,59 @@ const FetchUserData = () => {
           placeholder="Enter GitHub username"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
-         
+
         />
         <button type="submit">Submit</button>
       </form>
 
-     
+
 
       <div className="container">
-       {/* Display fetched data */}
-      {Object.keys(result).length > 0 && (
-        <div>
-          <h2>User Data</h2>
-          
-          <pre>{JSON.stringify(result, null, 2)}</pre>
-        </div>
-      )}
+       
+
+        {Object.keys(result).length > 0 ? (
+  <Card sx={{ maxWidth: 345 }}>
+    <CardMedia
+  sx={{
+    height: 100,
+    width: 100, // Set the width to 100px
+    borderRadius: '50%',
+  }}
+  image={`https://avatars.githubusercontent.com/u/${result.id}`}
+  title="User Avatar"
+/>
+
+
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="div">
+        {result.name} <Chip label={result.company} variant="outlined" />
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        {result.bio}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Link href={result.blog} target="_blank">
+        <Button variant="contained" color="success" size="small">
+          Portfolio
+        </Button>
+      </Link>
+      <Link href={result.html_url} target="_blank">
+        <Button variant="contained" color="success" size="small">
+          Github
+        </Button>
+      </Link>
+    </CardActions>
+  </Card>
+) : (
+   
+<Alert severity="info" style={{ marginTop: '3%' }}>
+  Please enter user or check the correct user name
+</Alert>
+)}
+
+
+
       </div>
     </>
   );
